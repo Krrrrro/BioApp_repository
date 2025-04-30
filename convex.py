e81fb6741cd178986e05aa40a895293cca4c6bcf
Krrrrro <dltmdgh4919@gmail.com>
2025-04-30 11:34:11 +0900

    markdown

--- convex.py
+++ convex.py
@@ -2,7 +2,7 @@ import numpy as np
 import open3d as o3d
 import trimesh
 
-pcd = o3d.io.read_point_cloud("result/" + "08b5f3d71-8750-4236-a024-b92189989cba.ply")
+pcd = o3d.io.read_point_cloud("result/" + "08b5f3d71-8750-4236-a024-b92189989cba11.ply")
 points = np.asarray(pcd.points)
 
 # 삼각형 메쉬로 변환
